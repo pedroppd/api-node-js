d0a6d25fdbd80aa99865ede411e37e2b9a43d022
pedroppd <pedro_pdantas@hotmail.com>
2021-06-15 08:53:19 -0300

    Criando end points

--- controller/CookieController.js
+++ controller/CookieController.js
@@ -5,4 +5,24 @@ module.exports = app => {
     app.get('/list', async (req, res) => {
             await res.send(cookieRepository.findAll());
     });
+
+    app.post('/', async () => {
+        
+
+    })
+
+    app.get('/:id', async () => {
+       
+        
+    })
+
+    app.put('/:id', async () => {
+    
+        
+    })
+
+    app.delete('/:id', async () => {
+        
+        
+    })
 }
