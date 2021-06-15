9d69a5d5b1ebd976e3714f6067c7e517c9fb62af
pedroppd <pedro_pdantas@hotmail.com>
2021-06-15 08:49:56 -0300

    Adicionando asyn/await

--- controller/CookieController.js
+++ controller/CookieController.js
@@ -2,7 +2,7 @@ const cookieRepository = require('../repository/CookieRepository')
 
 module.exports = app => {
 
-    app.get('/list', (req, res) => {
-            res.send(cookieRepository.findAll());
+    app.get('/list', async (req, res) => {
+            await res.send(cookieRepository.findAll());
     });
 }
