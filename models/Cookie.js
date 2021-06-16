410d33da4dcf3cc4e4b136031e9ef969b4eb73b5
pedroppd <pedro_pdantas@hotmail.com>
2021-06-15 21:13:16 -0300

    Criando model do Cookie

---
+++ models/Cookie.js
@@ -0,0 +1,13 @@
+class Cookie {
+    constructor({name, flavor, price, expiration_date, weight})
+    {
+        this.name = name;
+        this.flavor = flavor;
+        this.price = price;
+        this.expiration_date = expiration_date;
+        this.weight = weight;
+    }
+
+}
+
+module.exports = Cookie;
